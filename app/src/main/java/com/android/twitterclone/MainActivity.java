0ecb0b7cf1b2e3cac79114ea51f8ccd43d3ff606
Csegota <csegota406@gmail.com>
2019-07-03 17:46:40 -0700

    Git init & Parse server setup

---
+++ app/src/main/java/com/android/twitterclone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.twitterclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.ParseInstallation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
